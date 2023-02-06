f3edbbe9c8b4d14f3e245b2fc7f83d8229418e6b
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-06 14:01:55 +0000

    finito orario

--- oreg.py
+++ oreg.py
@@ -5,25 +5,30 @@ import datetime
 adesso = int(datetime.datetime.now().time().hour)
 if adesso >=0:
     buon="buongiorno"
-elif adesso>=12:
+    immag="static/images/buongiorno.jpg"
+if adesso>=12:
      buon="buon pranzo"
-elif adesso>=16:
+     immag="static/images/pranzo.jpg"
+if adesso>=16:
      buon="buon pomeriggio"
-elif adesso>=19:
+     immag="static/images/pomeriggio.jpg"
+if adesso>=19:
      buon="buona cena"
-elif adesso>=22:
+     immag="static/images/cena.jpg"
+if adesso>=22:
      buon="buona notte"
+     immag="static/images/sera.jpg"
 
 
 @app.route('/')
 @app.route('/en')
 def hello_world():
-  return render_template("indexcss.html", Titolo='Welcome', Testo='Hello, world!',immagine='static/images/download.png')
+  return render_template("indexcss.html", Titolo='Welcome', Testo=buon,immagine=immag)
 
 
 @app.route('/it')
 def ciao_mondo():
-  return render_template("indexcss.html", Titolo='Benvenuti', Testo=buon,immagine='static/images/download.png')
+  return render_template("indexcss.html", Titolo='Benvenuti', Testo=buon,immagine=immag)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
