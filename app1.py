d0a43a9212ff8d5c4178f14cd0c465a8e554342b
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-02-13 12:40:19 +0000

    modificaro e tolto alcuni file

--- app1.py
+++ app1.py
@@ -5,11 +5,14 @@ print("ciao ")
 @app.route('/', methods=['GET'])
 def hello_world():
     x=1
-    return(str(x+1)+"<br>"+'<h1 style="color:#ff0000">ciao, mondo!</h1>')
+    return(str(x+1)+"<br>"+'<h1 style="color:#ff0000">scrivi it o en dopo lo /</h1>')
 
 @app.route('/en', methods=['GET'])
 def inglese():
-    return("hello word,ciao, fdddd")
+    return("hello word!")
 
+@app.route('/it', methods=['GET'])
+def ita():
+    return("ciao mondo!")
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
